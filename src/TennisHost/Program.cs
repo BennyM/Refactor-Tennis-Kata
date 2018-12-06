25a3a6014adf7c63555fc8941aaa56756bbb6825
BennyM <bennym@me.com>
2018-12-06 09:50:36 +0100

    Added code sample

---
+++ src/TennisHost/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Tennis;
+
+namespace TennisHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Belgian Open");
+            string player1 = "Rafael Nadal";
+            string player2 = "Roger Federer";
+            Game game = new Game(player1, player2);
+            Random r = new Random();
+            string score;
+            while (!(score = game.GetScore()).Contains("Win"))
+            {
+                int v = r.Next(0, 101);
+                if (v % 2 == 0)
+                {
+                    Console.WriteLine("Point for player 1");
+                    game.WonPoint(player1);
+                }
+                else
+                {
+                    Console.WriteLine("Point for player 2");
+                    game.WonPoint(player2);
+                }
+                score = game.GetScore();
+                Console.WriteLine(score);
+            };
+        }
+    }
+}
